72c2bb06008eec359d4d02b1e1d9965c2390cab9
jc1765@york.ac.uk <jc1765@york.ac.uk>
2019-07-27 11:48:04 +0100

    moved the format_data function into an io module, in a project-specific package

--- solar-system.py
+++ solar-system.py
@@ -17,6 +17,7 @@ import sys
 import numpy as np
 import pandas as pd
 
+from solar import io
 
 # ----------------------- DEFINING ASTRONOMICAL UNITS ----------------------- #
 # metres, kilograms, seconds:
@@ -65,67 +66,6 @@ def accelerate(position, mass):
         axis=0)
     
     return acc
-
-
-# --------------------------- FORMATTING THE DATA --------------------------- #
-def format_data(pos_data, vel_data, index, column, num_iter, cur_date):
-    """Takes position and velocity arrays of shape nbod x ndim x nt
-    (bodies, spatial dimensions, time) and returns a DataFrame of the
-    data, sorted column-wise.
-    
-    Keyword arguments:
-    pos_data -- (nbod x ndim x nt array) position data
-    vel_data -- (nbod x ndim x nt array) velocity data
-    index -- Pandas.MultiIndex for a single date
-    column -- (array-like) column headers for the formatted DataFrame
-    num_iter -- (int) number of dates over which the data represents
-    cur_date -- (Pandas.DateTime) the date at the start of the data
-    
-    Returns:
-    cur_traj -- the formatted DataFrame 
-    """
-    
-    # Create a datetime array from start to finish.
-    date_range = cur_date + pd.to_timedelta(np.arange(num_iter), 'D') 
-
-    body_order = index.levels[0][index.codes[0]]
-
-    index.set_levels(date_range, level=1, inplace=True)  # set dates new index
-    index.set_codes(
-        [
-            np.tile(  # map planet name to number
-                index.codes[0],
-                num_iter),
-                
-            np.repeat(  # apply date to planets at each timestep
-                np.arange(num_iter),
-                len(index.codes[1]))
-        ],
-        verify_integrity=False,  # allow index to be erroneous for now
-        inplace=True
-    )
-
-    # Reshapes 3d arrays of to 2d, making time axis add repeating rows.
-    # position:
-    pos_data = pos_data.transpose(2,0,1).reshape(  # moves t axis to front
-            # rows are each body repeated for every timestep:
-            pos_data.shape[0] * pos_data.shape[2],
-            # columns give x & y pos for each body:
-            pos_data.shape[1])
-    
-    # same for velocity:
-    vel_data = vel_data.transpose(2,0,1).reshape(
-            vel_data.shape[0] * vel_data.shape[2],
-            vel_data.shape[1])
-
-    traj_data = np.hstack((pos_data, vel_data))  # pos & vel data in adj cols
-
-    # Puts it all together to create the DataFrame.
-    cur_traj = pd.DataFrame(data=traj_data, index=index, columns=column)
-    cur_traj.sort_index(inplace=True)
-    cur_traj = cur_traj.reindex(body_order, level='Name')
-    
-    return cur_traj
 
 
 # ---------------------- SETTING UP THE LOOP VARIABLES ---------------------- #
@@ -192,7 +132,7 @@ for step in range(1, num_steps):
         cur_date = start_date + pd.to_timedelta(step - cntr, 'D')
         
         if not last_step:
-            cur_traj = format_data(
+            cur_traj = io.format_data(
                 pos[:, :, :-1], vel[:, :, :-1],
                 index.copy(), columns,
                 cntr_change, cur_date)
@@ -200,7 +140,7 @@ for step in range(1, num_steps):
             pos = pos[:, :, -1]
             vel = vel[:, :, -1]
         else:
-            cur_traj = format_data(
+            cur_traj = io.format_data(
                 pos, vel,
                 index.copy(), columns,
                 cntr_change, cur_date)
---
+++ solar/io.py
@@ -0,0 +1,65 @@
+"""
+"""
+
+import numpy as np
+import pandas as pd
+
+# --------------------------- FORMATTING THE DATA --------------------------- #
+def format_data(pos_data, vel_data, index, column, num_iter, cur_date):
+    """Takes position and velocity arrays of shape nbod x ndim x nt
+    (bodies, spatial dimensions, time) and returns a DataFrame of the
+    data, sorted column-wise.
+    
+    Keyword arguments:
+    pos_data -- (nbod x ndim x nt array) position data
+    vel_data -- (nbod x ndim x nt array) velocity data
+    index -- Pandas.MultiIndex for a single date
+    column -- (array-like) column headers for the formatted DataFrame
+    num_iter -- (int) number of dates over which the data represents
+    cur_date -- (Pandas.DateTime) the date at the start of the data
+    
+    Returns:
+    cur_traj -- the formatted DataFrame 
+    """
+    
+    # Create a datetime array from start to finish.
+    date_range = cur_date + pd.to_timedelta(np.arange(num_iter), 'D') 
+
+    body_order = index.levels[0][index.codes[0]]
+
+    index.set_levels(date_range, level=1, inplace=True)  # set dates new index
+    index.set_codes(
+        [
+            np.tile(  # map planet name to number
+                index.codes[0],
+                num_iter),
+                
+            np.repeat(  # apply date to planets at each timestep
+                np.arange(num_iter),
+                len(index.codes[1]))
+        ],
+        verify_integrity=False,  # allow index to be erroneous for now
+        inplace=True
+    )
+
+    # Reshapes 3d arrays of to 2d, making time axis add repeating rows.
+    # position:
+    pos_data = pos_data.transpose(2,0,1).reshape(  # moves t axis to front
+            # rows are each body repeated for every timestep:
+            pos_data.shape[0] * pos_data.shape[2],
+            # columns give x & y pos for each body:
+            pos_data.shape[1])
+    
+    # same for velocity:
+    vel_data = vel_data.transpose(2,0,1).reshape(
+            vel_data.shape[0] * vel_data.shape[2],
+            vel_data.shape[1])
+
+    traj_data = np.hstack((pos_data, vel_data))  # pos & vel data in adj cols
+
+    # Puts it all together to create the DataFrame.
+    cur_traj = pd.DataFrame(data=traj_data, index=index, columns=column)
+    cur_traj.sort_index(inplace=True)
+    cur_traj = cur_traj.reindex(body_order, level='Name')
+    
+    return cur_traj
